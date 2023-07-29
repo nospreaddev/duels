b31376b762402ef15deb3431982cfb874daf38ac
sphhax <zskaistis2@gmail.com>
2023-07-28 22:24:08 -0500

    added some help text to spawn entity

--- code/entities/ArenaPlayerSpawn.cs
+++ code/entities/ArenaPlayerSpawn.cs
@@ -14,7 +14,7 @@ public class SpawnPoint : Entity
 	public int ArenaNumber { get; set; } = 0;
 
 	/// <summary>
-	/// The side this spawn point is on in it's arena. Each arena should have an A and B spawnpoint.
+	/// The side this spawn point is on in it's arena. Each arena should have an A and B spawnpoint. Put A or B in this box depending on which side it is. This may change in the future.
 	/// </summary>
 	[Property( Title = "Arena Side" )]
 	public List<string> ArenaSide { get; set; } = new List<string>
