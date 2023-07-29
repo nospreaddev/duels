ad58f9c2089bc67d064147b330b277472fd636c9
sphhax <zskaistis2@gmail.com>
2023-07-28 22:16:14 -0500

    added arena specific and side specific spawn entities

---
+++ code/entities/ArenaPlayerSpawn.cs
@@ -0,0 +1,24 @@
+﻿using Sandbox;
+using Editor;
+using System.Collections.Generic;
+
+[Library( "info_player_start" ), HammerEntity]
+[Title( "Player Spawnpoint" ), Category( "Player" ), Icon( "place" )]
+public class SpawnPoint : Entity
+{
+	/// <summary>
+	/// Arena Number
+	/// </summary>
+	[Property( Title = "Arena Number" )]
+	public int ArenaNumber { get; set; } = 0;
+
+	/// <summary>
+	/// The side this spawn point is on in it's arena. Each arena should have an A and B spawnpoint.
+	/// </summary>
+	[Property( Title = "Arena Side" )]
+	public List<string> ArenaSide { get; set; } = new List<string>
+	{
+		"A",
+		"B",
+	};
+}
